5785f0ed78a3faa9f76e8067a59dabf4c9d13492
Melissa Huff <m_huff@mail.fhsu.edu>
2024-04-10 11:32:54 -0700

    Created Instance File using flask --app flaskr init-db

--- flaskr/db.py
+++ flaskr/db.py
@@ -3,11 +3,18 @@ import sqlite3
 import click
 from flask import current_app, g
 
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
 def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
+
 
 @click.command('init-db')
 def init_db_command():
